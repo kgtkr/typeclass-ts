bc8e66b0fdb4b202bf70dbd92728f5f5e0be8c2c
kgtkr <kgtkr.jp@gmail.com>
2018-09-14 22:37:58 +0900

    具体例的な物

---
+++ src/sample.ts
@@ -0,0 +1,55 @@
+const prototypeSymbol = Symbol();
+interface TypeClass<T> {
+  [prototypeSymbol]: T
+}
+
+const showSymbol = Symbol();
+interface showTypeProps<T> {
+  show: (self: T) => string
+}
+interface showTypePrototype<T> {
+  [showSymbol]: showTypeProps<T>
+}
+interface showType<T> extends TypeClass<showTypePrototype<T>> { };
+function show<T extends showType<T>>(self: T): string {
+  return self[prototypeSymbol][showSymbol].show(self);
+}
+
+const eqSymbol = Symbol();
+interface eqTypeProps<T> {
+  eq: (self: T, other: T) => boolean
+}
+interface eqTypeProtoType<T> {
+  [eqSymbol]: eqTypeProps<T>
+}
+interface eqType<T> extends TypeClass<eqTypeProtoType<T>> { };
+function eq<T extends eqType<T>>(self: T, other: T): boolean {
+  return self[prototypeSymbol][eqSymbol].eq(self, other);
+}
+
+interface MyDataProps {
+  x: number,
+  y: number
+}
+interface MyDataPrototype extends showTypePrototype<MyData>, eqTypeProtoType<MyData> {
+}
+interface MyData extends MyDataProps, TypeClass<MyDataPrototype> {
+};
+const MyDataShowInstance: showTypeProps<MyData> = {
+  show: self => `(${self.x},${self.y})`
+};
+const MyDataEqInstance: eqTypeProps<MyData> = {
+  eq: (self, other) => self.x === other.x && self.y === other.y
+};
+const MyDataPrototype: MyDataPrototype = {
+  [showSymbol]: MyDataShowInstance,
+  [eqSymbol]: MyDataEqInstance
+};
+function MyData(props: MyDataProps): MyData {
+  return {
+    ...props,
+    [prototypeSymbol]: MyDataPrototype
+  };
+}
+
+show(MyData({ x: 1, y: 2 }))
