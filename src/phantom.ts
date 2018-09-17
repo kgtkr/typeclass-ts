f6e9c2c0be089f946139e53f586a4ee2922b5027
kgtkr <kgtkr.jp@gmail.com>
2018-09-17 17:18:37 +0900

    色々消したり

--- src/phantom.ts
+++
@@ -1,6 +0,0 @@
-export type PhantomType<T> = (x: T) => T[];
-const phantomType: PhantomType<any> = () => [];
-export function PhantomType<T>(): PhantomType<T> {
-  return phantomType;
-}
-export type PhantomTypeUnwrap<T extends PhantomType<any>> = T extends PhantomType<infer P> ? P : never;
